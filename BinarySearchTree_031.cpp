4b9aca30efc22f000d43c701016b072b39b42157
Izalhadji31 <ryzalhadji@gmail.com>
2023-06-29 15:24:40 +0700

    jika nilai di bidang data node baru lebih besar dari induknya

--- BinarySearchTree_031.cpp
+++ BinarySearchTree_031.cpp
@@ -49,3 +49,4 @@ public:
 		{
 			parent->leftchild = newNode; // Make the left child of the parent point to the new node
 		}
+		else if (element > parent->info) // if the value in the data field of the new node is greater than that of the parent
