5baa2e8e3fb2f87824ef0ce2aa647681f7672eb7
Izalhadji31 <ryzalhadji@gmail.com>
2023-06-29 15:18:11 +0700

    lokasi node akan menjadi induk dari node yang akan disisipkan

--- BinarySearchTree_031.cpp
+++ BinarySearchTree_031.cpp
@@ -36,4 +36,5 @@ public:
 		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
 
 		Node* parent = NULL;
-		Node* currentNode = NULL
+		Node* currentNode = NULL;
+		search(element, parent, currentNode); // Locate the node will be the parent of the node to be inserted 
